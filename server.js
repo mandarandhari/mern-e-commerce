291f9da53bf6a61d575b7b179aa0dbb1aaa8414e
Mandar <mandarandhari1996@gmail.com>
2020-09-20 12:40:39 +0530

    server js changes

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ connectDB();
 app.use(express.json({ extended: true }));
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(serveStatic(path.join(__dirname, 'client', 'build')));
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-    });
-}
-
 app.use('/register', require(path.join(__dirname, './routes/customers/register')));
 app.use('/login', require(path.join(__dirname, './routes/customers/login')));
 app.use('/customer', require(path.join(__dirname, './routes/customers/customer')));
@@ -30,6 +22,14 @@ app.use('/my-orders', require(path.join(__dirname, './routes/myorders')));
 app.use('/addCountries', require(path.join(__dirname, './routes/countries')));
 app.use('/contact', require(path.join(__dirname, './routes/contact')));
 app.use('/reset-password', require(path.join(__dirname, './routes/customers/reset_password')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(serveStatic(path.join(__dirname, 'client', 'build')));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started at port ${port}`));
